fc439c5ea828837662d5a1c430403f73a5b9d012
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-03-30 19:24:40 +0200

    chart js sample

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import s from './App.module.css';
+// import { Line } from 'react-chartjs-2';
+import Chart from './components/Chart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+
+  render() {
+
+    return (
+      <div className={s.App}>
+         <Chart/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Chart.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
+import s from './Chart.module.css';
+
+
+class Chart extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: {
+                labels: ['Moscow', 'Lobnya', 'Dubna', 'Karachi', 'Tula', 'Islamabad'],
+                datasets: [
+                    {
+                        label: 'Population',
+                        data: [
+                            117594,
+                            181045,
+                            153060,
+                            106519,
+                            105162,
+                            95072
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)',
+                            'rgba(255, 159, 64, 0.6)',
+                            'rgba(255, 99, 132, 0.6)'
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className={s.chart}>
+                <Bar
+                    data={this.state.chartData}
+                    options={{}}
+                />
+            </div>
+
+        )
+    }
+}
+export default Chart;
